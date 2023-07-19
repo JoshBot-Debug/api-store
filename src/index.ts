972859762c0541fe152f8b38a2e6e8c19ad6ebbf
JoshBot-Debug <joshuamyerswebdev@gmail.com>
2023-07-19 22:02:04 +0530

    added useInfiniteQuery and getData function in both useQuery and infiniteQUery

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { createModel, createTable } from "./model";
 import { useInfiniteQuery } from "./useInfiniteQuery";
 import { useMutation } from "./useMutation";
 import { useQuery } from "./useQuery";
+import { UseAPIStore } from "./types";
 
 export {
+  type UseAPIStore,
   APIStore,
   createTable,
   createModel,
